342e2c6c7f1cfb9f827a0f819ec7345c7fafa26c
sudeshna0987 <sudheshna.paduchuri@gmail.com>
2023-04-20 18:43:47 +0530

    sql script for adding employee table

---
+++ sql practice/1-create tables.sql
@@ -0,0 +1,12 @@
+create table employee
+(
+    e_id int,
+    firstname varchar(100),
+    lastname varchar(200),
+    dept_id int,
+    gender char,
+    servicehistory_id int,
+    projectlevel_id int,
+    employee_code int
+) 
+
